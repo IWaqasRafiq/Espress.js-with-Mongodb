b7a9b1dfdf67c63080c16efbba0f4945b0dcdfec
waqas rafiq <wm66179@gmail.com>
2023-07-27 01:35:13 +0500

    mongodb crud operation

---
+++ server.mjs
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+const __dirname = path.resolve();
+
+import authRouter from './routes/auth.mjs'
+import postRouter from './routes/post.mjs'
+
+
+
+
+const app = express();
+app.use(express.json()); // body parser
+// app.use(cors())
+
+// /api/v1/login
+app.use("/api/v1", authRouter)
+
+
+app.use((req, res, next) => {
+    let token = "valid"
+    if (token === "valid") {
+        next();
+    } else {
+        res.send({ message: "invalid token" })
+    }
+})
+
+
+app.use("/api/v1", postRouter)
+
+
+
+
+//     /static/vscode_windows.exe
+app.use("/static", express.static(path.join(__dirname, 'static')))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 5501;
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
